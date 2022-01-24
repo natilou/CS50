f0bb4b37fcc6f29a800e91ee3ee65b08a9adf7cb
natilou <30585029+natilou@users.noreply.github.com>
2022-01-24 20:07:03 -0300

    Project 2: implementacion Models

--- Project 2/commerce/auctions/models.py
+++ Project 2/commerce/auctions/models.py
@@ -4,3 +4,39 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Bid(models.Model):
+    starting_amount = models.FloatField()
+    currency = models.CharField(max_length=4) 
+
+    def __str__(self) -> str:
+        return f"{self.currency} {self.starting_amount}"
+    
+
+class Comment(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_comments")
+    textarea = models.TextField(max_length=200)
+
+    def __str__(self) -> str:
+        return f"Comment by {self.user}, {self.textarea}"
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField(max_length=300)
+    starting_bid = models.ForeignKey(Bid, on_delete=models.CASCADE, related_name="bids")
+    url_img = models.URLField()
+    category = models.CharField(max_length=64)
+    is_active = models.BooleanField(default=True)
+    user = models.OneToOneField(User,  on_delete=models.CASCADE, related_name="user_listings")
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name="listing_comments")
+    
+    @property
+    def status_label(self):
+        if not self.is_active:
+            return "Closed"
+        return "Active" 
+
+
+    def __str__(self) -> str:
+        return f"{self.title}, starting bid: {self.starting_bid}, {self.category}, {self.status_label}"
