7cadddb52c3c3db413203f1bcb3e0fbc80b27328
natilou <30585029+natilou@users.noreply.github.com>
2022-02-28 20:47:38 -0300

    Project 3: Implementacion Reply

--- Project 3/mail/mail/static/mail/inbox.js
+++ Project 3/mail/mail/static/mail/inbox.js
@@ -59,9 +59,7 @@ function load_mailbox(mailbox) {
   document.querySelector("#compose-view").style.display = "none";
 
   // Show the mailbox name
-  document.querySelector("#emails-view").innerHTML = `<h3>${
-    mailbox.charAt(0).toUpperCase() + mailbox.slice(1)
-  }</h3>`;
+  document.querySelector("#emails-view").innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
   //Show new mails
   fetch(`/emails/${mailbox}`)
@@ -90,41 +88,32 @@ function show_mails(email, mailbox) {
   }
   mail.innerHTML = `
   <div class="col-3" id="user-div">${user}</div>
-  <div class="col-4">${email.subject}</div>
+  <div class="col-4" id="subject-div" style="cursor:pointer;">${email.subject}</div>
   <div class="col-3">${email.timestamp}</div>`;
   document.querySelector("#emails-view").append(mail);
   mail.style.color = "black";
   mail.append(buttonColumn);
 
-  // button unread mail
-  if (
-    (mailbox === "inbox" && email.read) ||
-    (mailbox === "archive" && email.read)
-  ) {
+  // button read or unread mail
+  if (mailbox === "inbox" || mailbox === "archive") {
     const readButton = document.createElement("button");
     readButton.id = "read-button";
     readButton.className = "btn btn-sm btn-outline-primary";
-    readButton.innerHTML = "Unread";
+    readButton.innerHTML = email.read
+      ? `<i class="bi bi-envelope" data-toggle="tooltip" data-placement="bottom" title="Mark as unread"></i>`
+      : `<i class="bi bi-envelope-open" data-toggle="tooltip" data-placement="bottom" title="Mark as read"></i>`;
     buttonColumn.append(readButton);
 
     readButton.addEventListener("click", () => {
-      fetch(`emails/${email.id}`, {
-        method: "PUT",
-        body: JSON.stringify({
-          read: false,
-        }),
-      }).then((result) => {
-        console.log(result);
-        load_mailbox("inbox");
-      });
+      changeEmailReadStatus(email, mailbox);
     });
   }
 
   //click on email to read it
 
-  mail.querySelector("#user-div").addEventListener("click", () => {
+  mail.querySelector("#subject-div").addEventListener("click", () => {
     console.log(email.id);
-    single_email(`${email.id}`);
+    readEmail(email, mailbox);
   });
 
   // change background color if emails is readed
@@ -132,57 +121,82 @@ function show_mails(email, mailbox) {
     mail.style.backgroundColor = "white";
   } else {
     mail.style.backgroundColor = "#f4f4f5";
+    mail.style.fontWeight = "bold";
   }
 
   // archive mail
   if (mailbox === "inbox" || mailbox === "archive") {
     const archiveButton = document.createElement("button");
     archiveButton.className = "btn btn-sm btn-outline-primary";
-    archiveButton.innerHTML = `${email.archived ? "Unarchived" : "Archive"}`;
+    archiveButton.innerHTML = email.archived
+      ? `<i class="bi bi-inbox" data-toggle="tooltip" data-placement="bottom" title="Unarchive"></i>`
+      : `<i class="bi bi-archive data-toggle="tooltip" data-placement="bottom" title="Archive"></i>`;
     buttonColumn.append(archiveButton);
 
     archiveButton.addEventListener("click", () => {
-      archive_email(email);
+      changeEmailArchiveStatus(email);
     });
   }
 }
 
 // view and read one email
 
-function single_email(email_id) {
+function readEmail(email, mailbox) {
   document.querySelector("#emails-view").style.display = "block";
   document.querySelector("#compose-view").style.display = "none";
 
-  fetch(`emails/${email_id}`)
+  fetch(`emails/${email.id}`)
     .then((response) => response.json())
     .then((email) => {
-      read_email(email_id);
+      if (!email.read) {
+        changeEmailReadStatus(email);
+      }
       console.log(email);
       document.querySelector("#emails-view").innerHTML = `
-    <div>From: ${email.sender}</div>
-    <div>To: ${email.recipients}</div>
-    <div>Subject: ${email.subject}</div> 
-    <div>Time: ${email.timestamp} </div>
-    <div> ${email.body} </div>
-    `;
+      <dl class="row">
+        <dt class="col-sm-1">From:</dt> 
+        <dd class="col-sm-11">${email.sender}</dd>
+        <dt class="col-sm-1">To:</dt>
+        <dd class="col-sm-11">${email.recipients}</dd>
+        <dt class="col-sm-1">Subject:</dt> 
+        <dd class="col-sm-11">${email.subject}</dd> 
+        <dt class="col-sm-1">Time:</dt> 
+        <dd class="col-sm-11">${email.timestamp}</dd> 
+        <dt class="col-sm-1">Content:</dt> 
+        <dd class="col-sm-11"> <textarea readonly style="width:100%;"> ${email.body} </textarea></dd>  
+      </dl>`;
+      if (mailbox === "inbox" || mailbox === "archive") {
+        const replyButton = document.createElement("button");
+        replyButton.className = "btn btn-sm btn-outline-primary";
+        replyButton.innerHTML = "Reply";
+        document.querySelector("#emails-view").append(replyButton);
+    
+        replyButton.addEventListener("click", () => {
+          replyEmail(email);
+        });
+      }
     });
+    
 
   return false;
 }
 // change read status
 
-function read_email(email_id) {
-  fetch(`/emails/${email_id}`, {
+function changeEmailReadStatus(email, mailbox) {
+  fetch(`/emails/${email.id}`, {
     method: "PUT",
     body: JSON.stringify({
-      read: true,
+      read: !email.read,
     }),
+  }).then((result) => {
+    console.log(result);
+    load_mailbox(mailbox);
   });
 }
 
 // change archive status
 
-function archive_email(email) {
+function changeEmailArchiveStatus(email) {
   if (!email.archived) {
     fetch(`/emails/${email.id}`, {
       method: "PUT",
@@ -204,4 +218,12 @@ function archive_email(email) {
       load_mailbox("inbox");
     });
   }
+}
+
+// reply email
+function replyEmail(email) {
+  compose_email();
+  document.querySelector("#compose-recipients").value = email.sender;
+  document.querySelector("#compose-subject").value = email.subject.startsWith("Re:") ? email.subject : `Re: ${email.subject}`;
+  document.querySelector("#compose-body").value = `On ${email.timestamp}, ${email.sender} wrote: ${email.body}`;
 }
